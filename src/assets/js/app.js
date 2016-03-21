cf400ab99ecdca6635ad50ba1291c36e076dbd72
joshkissel <Josh@Joshs-MacPro.local>
2016-03-21 16:34:33 -0600

    fixing js bug

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -1,5 +1,20 @@
 $(document).foundation();
 
+$(function() {
+$('a[href*="#"]:not([href="#"])').click(function() {
+  if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+    var target = $(this.hash);
+    target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+    if (target.length) {
+      $('html, body').animate({
+        scrollTop: target.offset().top
+      }, 1000);
+      return false;
+    }
+  }
+});
+});
+
 // enabling headroom
 var myElement = document.querySelector(".paradeiser");
 var headroom  = new Headroom(myElement, {
